7384b45c149ddea2c7f21751657ed735014dde16
masingh09 <manishasingh.01985@gmail.com>
2024-03-20 15:09:26 +1100

    adding methods with different timeComplexity

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        //Time Complexity
+        System.out.println("Hello and welcome!"); //O(1) - Constant
+        printNumbersWithLoops();
+        printNumbersWithNestedLoops();
+    }
 
+    private static void printNumbers() {
+        // We avoid constants from final calculation
+        // Overall time complexity = O(1 + n) = O(n)
+        System.out.println("Hello and welcome!"); //O(1) - Constant
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = "+ (i+5));
-
+            System.out.println("i = "+ i); // O(n) - Linear
         }
+    }
+    private static void printNumbersWithLoops() {
+        // Overall time complexity = O(n + n) = O(n)
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = "+ i); // O(n) - Linear
+        }
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("i = "+ (i+5)); // O(n) - Linear
+        }
+    }
+
+    private static void printNumbersWithNestedLoops() {
+        // Overall time complexity = O(n * n) = O(n^2)
+        for (int i = 1; i <= 5; i++) {
+            for (int j=1; j<= 5; j++) {
+                System.out.println("i + j = " + i+j); // O(n^2) - Quadratic
+            }
+        }
+
     }
 }
