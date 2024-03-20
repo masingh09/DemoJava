012338ce891e70d770b517ed1866de36269891e9
masingh09 <manishasingh.01985@gmail.com>
2024-03-20 15:15:57 +1100

    additional comments to the code

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,22 @@ public class Main {
     public static void main(String[] args) {
         //Time Complexity
         System.out.println("Hello and welcome!"); //O(1) - Constant
-        printNumbersWithLoops();
-        printNumbersWithNestedLoops();
+        printNumbers(); // O(n)
+        printNumbersWithLoops(); //O(n)
+        printNumbersWithNestedLoops(); //O(n^2)
     }
 
     private static void printNumbers() {
         // We avoid constants from final calculation
         // Overall time complexity = O(1 + n) = O(n)
-        System.out.println("Hello and welcome!"); //O(1) - Constant
+        System.out.println("call from printNumbers!"); //O(1) - Constant
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = "+ i); // O(n) - Linear
         }
     }
     private static void printNumbersWithLoops() {
-        // Overall time complexity = O(n + n) = O(n)
+        // Overall time complexity = O(1+ n + n) = O(1+2n) = O(n)
+        System.out.println("call from printNumbersWithLoops!"); //O(1) - Constant
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = "+ i); // O(n) - Linear
         }
@@ -26,9 +28,10 @@ public class Main {
 
     private static void printNumbersWithNestedLoops() {
         // Overall time complexity = O(n * n) = O(n^2)
+        System.out.println("call from printNumbersWithNestedLoops!"); //O(1) - Constant
         for (int i = 1; i <= 5; i++) {
             for (int j=1; j<= 5; j++) {
-                System.out.println("i + j = " + i+j); // O(n^2) - Quadratic
+                System.out.println("i + j = " + (i+j)); // O(n^2) - Quadratic
             }
         }
 
